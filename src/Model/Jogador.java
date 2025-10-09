d2240a63bff54b0191238ea2cfd635909abf5aeb
Barbssz <arthurbarbosacomp@gmail.com>
2025-10-09 20:44:25 -0300

    Starting Jogador's Development

---
+++ src/Model/Jogador.java
@@ -0,0 +1,40 @@
+package Model;
+import java.util.*;
+
+enum Cor{
+     VERMELHO, AZUL, LARANJA, AMARELO, ROXO, CINZA;
+}
+
+class Jogador {
+    /*Propriedades de objeto*/
+    int qtdFugaPrisao;
+    boolean estaPreso;
+    float dinheiro;
+    Cor cor;
+    String nome;
+
+    /*Construtor */
+    public Jogador(Cor cor, String nome) {
+        this.cor = cor;
+        this.estaPreso = false;
+        this.dinheiro = 4000;
+        this.qtdFugaPrisao = 0;
+    }
+    
+    /*Função para gerar números entre min e max, incluindo os limites */
+    public int generateRandomInt(int min, int max){
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+    
+
+    /*Função que retorna um array com 2 elementos, representando o resultado dos lancamentos de cada dado */
+    public int[] lancarDados(){
+        int dados[];
+        dados = new int[2];
+        dados[0] = generateRandomInt(0, 6);
+        dados[1] = generateRandomInt(0, 6);
+        return dados;
+    }
+    
+}
