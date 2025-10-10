758222a6221f1f05d42f9ffd8625321cf06f709b
Barbssz <arthurbarbosacomp@gmail.com>
2025-10-10 17:10:05 -0300

    Colocando os arq de volta

---
+++ src/src/Model/Casa.java
@@ -0,0 +1,23 @@
+
+/** Criamos uma classe abstrata chamada "Casa", que representa
+ * um espaço no tabuleiro do jogo. Ela não poderá ser instanciada porque
+ * será herdada por outras classes, como Propriedade, Prisão, Sorte/Revés etc.
+ * 
+*/
+abstract class Casa{
+    private int posicao;
+    private String propriedade;
+
+    public Casa(int posicao, String propriedade){
+        this.posicao = posicao;
+        this.propriedade = propriedade;
+    }
+
+    public String getPropriedade(){
+        return this.propriedade;
+    }
+
+    public int getPosicao(){
+        return this.posicao;
+    }
+}
