e97710b557d9c2dfe4fd10c9a360f22607d86553
Barbssz <arthurbarbosacomp@gmail.com>
2025-10-09 21:05:30 -0300

    Creating Casa.java

---
+++ src/Model/Casa.java
@@ -0,0 +1,16 @@
+package Model;
+
+/** Criamos uma classe abstrata chamada "Casa", que representa
+ * um espaço no tabuleiro do jogo. Ela não poderá ser instanciada porque
+ * será herdada por outras classes, como Propriedade, Prisão, Sorte/Revés etc.
+ * 
+*/
+abstract class Casa{
+    int posicao;
+    String propriedade;
+
+    public Casa(int posicao, String propriedade){
+        this.posicao = posicao;
+        this.propriedade = propriedade;
+    }
+}
