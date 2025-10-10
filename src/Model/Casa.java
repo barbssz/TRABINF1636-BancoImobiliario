9f4e581229cd179079b345613731c9890a3cd1ba
Barbssz <arthurbarbosacomp@gmail.com>
2025-10-09 21:23:48 -0300

    Adding get methods to Casa.java

--- src/Model/Casa.java
+++ src/Model/Casa.java
@@ -1,4 +1,3 @@
-package Model;
 /** Criamos uma classe abstrata chamada "Casa", que representa
  * um espaço no tabuleiro do jogo. Ela não poderá ser instanciada porque
  * será herdada por outras classes, como Propriedade, Prisão, Sorte/Revés etc.
@@ -11,5 +10,13 @@ abstract class Casa{
     public Casa(int posicao, String propriedade){
         this.posicao = posicao;
         this.propriedade = propriedade;
+    }
+
+    public String getPropriedade(){
+        return this.propriedade;
+    }
+
+    public int getPosicao(){
+        return this.posicao;
     }
 }
